5390f951e197fe33857c3523b16e4aea341e9da5
smsohag32 <sohagsheik32@gmail.com>
2025-01-10 00:59:36 +0600

    contact page added

--- frontend/src/components/layouts/MainLayout/MainLayout.jsx
+++ frontend/src/components/layouts/MainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const MainLayout = ({ children }) => {
    return (
-      <div className="relative transition-all duration-300">
+      <div className="relative  transition-all duration-300">
          <Header />
          <div className="pt-16 min-h-[40vh]">
             {children}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,15 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Router'
 import { Provider } from 'react-redux'
 import store from './redux-store/store'
-import AuthProvider from './context/AuthProvider'
 
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
-      <AuthProvider>
-         <Provider store={store}>
-            <RouterProvider router={router} >
-            </RouterProvider>
+      <Provider store={store}>
+         <RouterProvider router={router} >
 
-         </Provider>
-      </AuthProvider>
+         </RouterProvider>
+
+      </Provider>
    </StrictMode>
 )
